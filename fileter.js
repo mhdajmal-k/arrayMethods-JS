6f7dbbfbbed160c351ac1b704937e6f5cfefe333
Muhammed Ajmal k <ajmalchundappuram@gmail.com>
2024-04-05 21:34:10 +0530

    filter method

---
+++ fileter.js
@@ -0,0 +1,20 @@
+//as name of filter it is a filter function in array 
+//create new array
+
+//filtering even numbers
+const numbers=[1,2,3,4,5,6,7,8,9,10]
+const evenNumbers=numbers.filter((elem)=>elem%2==0)
+console.log(evenNumbers);
+
+
+//duplicate elements
+const duplicate=[1,2,3,4,5,6,6,7,8,9,6,3,2,1]
+const nums=duplicate.filter((elem,index,ara)=>{
+    console.log(elem+"elem");
+    console.log(index,"it index");
+    console.log(ara.indexOf(elem));
+    return ara.indexOf(elem)===index
+
+})
+console.log(nums);
+
