3dc3d15e9e2124fcf35755a813ba5623cd74444f
Muhammed Ajmal k <ajmalchundappuram@gmail.com>
2024-04-06 11:59:02 +0530

    sort  method

---
+++ sort.js
@@ -0,0 +1,9 @@
+//sort number and strings
+//sorting is first compail with string
+//sorting names utf
+const names=['amal','ameen','yaseen','bilal','eachu','ziya']
+console.log(names.sort())
+
+const number=[1,2,4,5,67,7,8,4,23,5,7,8,5,3,3,1,5,8,8,79,65,999]
+const sort=number.sort((a,b)=>a-b)
+console.log(sort);
