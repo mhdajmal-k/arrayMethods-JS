34334bf66eae84c56466d4040fa2d2bd7e0ab056
Muhammed Ajmal k <ajmalchundappuram@gmail.com>
2024-04-06 12:04:05 +0530

    concat  method

---
+++ concate.js
@@ -0,0 +1,11 @@
+//merge two array to create a new array
+
+ 
+const a=[1,2,3,4,5]
+const b=[6,7,8,9,10]
+const c=a.concat(b)
+console.log(c);
+const d=[11,12,13,14,15]
+const e=a.concat(b,d)
+console.log(e);
+
